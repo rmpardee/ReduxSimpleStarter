88d5b2a17af40df57883e7b1c2aaffc82d290a0a
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-12-01 19:24:44 -0800

    API call successfully returning

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import API_KEY from '../config.js';
-
+import { API_KEY } from '../config.js';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,19 +3,21 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index.js';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
     // we need this so the context is preserved when calling onInputChange
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onFormSubmit(event) {
     // the browser wants to submit the form and refresh the HTML on submit.
     event.preventDefault();
-    // here we call the action creator
-
+    // here we call the action creator with the specific term
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   onInputChange(event) {
@@ -37,3 +39,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
